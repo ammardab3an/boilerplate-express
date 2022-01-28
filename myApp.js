fe2c520647a3f10f5be9344f436de1962c2ed92f
AmmarDab3an <ammar.dabaan99@gmail.com>
2022-01-28 21:50:14 +0200

    env fixed by removing .env from .gitignore

--- myApp.js
+++ myApp.js
@@ -9,14 +9,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-    
-    res.send(JSON.stringify({
-        "cwd": process.cwd(),
-        "dir": __dirname,
-        "_env": _env,
-        "env_all": process.env,
-        "env": process.env.MESSAGE_STYLE,
-    }, null, 4));
 
     const tmp = {"message": "Hello json"}
     if(process.env.MESSAGE_STYLE === 'uppercase'){
